aa5b06d2661e1ed5bc98ed3e4591c0d6407454e4
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-09-27 20:28:50 +0300

    fixed styles for menu

--- src/provider/theme/index.tsx
+++ src/provider/theme/index.tsx
@@ -27,7 +27,7 @@ export const ThemeProvider: FC<PropsWithChildren> = ({ children }) => {
           Menu: {
             darkItemBg: darkBgColor,
             darkItemSelectedBg: darkSelectedBgColor,
-            
+            darkPopupBg: darkBgColor,
             darkSubMenuItemBg: darkBgColor
           },
           Layout: {

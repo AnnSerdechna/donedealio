e0bf2fd129129dc67a11539ae40ab195e3c62c02
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-09-27 16:23:41 +0300

    project setup

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,14 @@
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import GoogleProvider from "next-auth/providers/google"
 
 import prisma from '../../../../../lib/prisma'
 
-// const authOptions: AuthOptions = {
-  // providers: [
-  //   GoogleProvider({
-  //     clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-  //     clientSecret: process.env.GOOGLE_SECRET ?? '',
-  //   }),
-  // ],
-  // adapter: PrismaAdapter(prisma),
-  // secret: process.env.NEXTAUTH_SECRET
-// };
-
-// const handler = NextAuth(authOptions);
-
-
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_SECRET ?? '',
-    }),
-  ],
+const authOptions: AuthOptions = {
+  providers: [],
   adapter: PrismaAdapter(prisma),
-  secret: process.env.NEXTAUTH_SECRET,
-})
+  secret: process.env.NEXTAUTH_SECRET
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { GoogleAuth } from '@/components/auth/google-auth';
 
 export default async function Home() {
   return (
     <main>
-      <GoogleAuth />
+      
     </main>
   );
 }

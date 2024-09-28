4bb3574e680239f96a5d03e1cef233636b5cd728
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-09-28 16:22:30 +0300

    fixed menu styles

--- src/app/[userId]/settings/page.tsx
+++ src/app/[userId]/settings/page.tsx
@@ -1,7 +1,7 @@
 export default function Settings() {
   return (
     <section>
-      Profile
+      Settings
     </section>
   );
 };
--- src/components/layout/tasks/menu/index.tsx
+++ src/components/layout/tasks/menu/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, Key } from 'react';
 import {
   SettingOutlined,
   PieChartOutlined,
@@ -16,11 +16,12 @@ import styles from './index.module.scss';
 type MenuItem = Required<MenuProps>['items'][number];
 
 type RoleType = 'ADMIN' | 'MEMBER' | 'DEFAULT';
+
 const role: RoleType = 'ADMIN'; 
 
 function getItem(
   label: React.ReactNode,
-  key: string,
+  key: Key,
   icon?: React.ReactNode,
   children?: MenuItem[],
 ): MenuItem {
@@ -29,12 +30,12 @@ function getItem(
   const adminMenu: string[] = ['dashboard', 'workspace', 'workspace?1', 'team', 'team?1', 'profile', 'settings'];
 
   if (role === 'MEMBER') {
-    if (!memberMenu.includes(key)) {
+    if (!memberMenu.includes(key as string)) {
       return null
     }
   }
 
-  if (role === 'ADMIN' && !adminMenu.includes(key)) {
+  if (role === 'ADMIN' && !adminMenu.includes(key as string)) {
     return null
   }
 
@@ -46,32 +47,38 @@ function getItem(
   } as MenuItem;
 }
 
+const userId = '1';
+
+const navigate = (href: string): string => {
+  return `/${userId}/${href}`
+};
+
 const items: MenuItem[] = [
   getItem(
-    'Dashboard', 
+    'Dashboard',
     'dashboard',
-    <Link href={'/dashboard'}>
+    <Link href={navigate('dashboard')}>
       <PieChartOutlined />
     </Link>
   ),
   getItem(
-    'Workspace', 
-    'workspace', 
-    <BulbOutlined />, 
+    'Workspace',
+    'workspace',
+    <BulbOutlined />,
     [getItem(
-    <Link href={'/workspace?1'}>
-      Workspace 1
-    </Link>,
-    'workspace?1'
+      <Link href={navigate('workspace/1')}>
+        Workspace 1
+      </Link>,
+      'workspace?1'
     )]),
   getItem(
-    'Team', 
-    'team', 
-    <TeamOutlined />, 
+    'Team',
+    'team',
+    <TeamOutlined />,
     [getItem(
-      <Link href={'/team?1'}>
+      <Link href={navigate('team/1')}>
         Team 1
-      </Link>, 
+      </Link>,
       'team?1'
     )]),
 ];
@@ -81,41 +88,44 @@ const bottomItems: MenuItem[] = [
   getItem(
     'Profile',
     'profile',
-    <Link href={'/profile'}>
+    <Link href={navigate('profile')}>
       <UserOutlined />
     </Link>
   ),
   getItem(
     'Settings',
     'settings',
-    <Link href={'/settings'}>
+    <Link href={navigate('settings')}>
       <SettingOutlined />
     </Link>
   ),
   { type: 'divider' },
 ]?.filter(Boolean) as MenuItem[]
 
-const MenuComponent: FC<{ items: MenuItem[]}> = ({items}) => {
+
+const MenuComponent: FC<{ items: MenuItem[] }> = ({ items }) => {
   const pathname = usePathname();
 
-  const selectedItem = pathname.slice(1);
+  const selectedMenuItems = (items: MenuItem[]) => items.filter(item => pathname.includes(item?.key as string)).map(item => item?.key as string)
 
   return (
     <Menu
       theme={'dark'}
       defaultSelectedKeys={['']}
-      defaultOpenKeys={[selectedItem]}
-      selectedKeys={[selectedItem]}
-      activeKey={selectedItem}
+      defaultOpenKeys={selectedMenuItems(items)}
+      selectedKeys={selectedMenuItems(items)}
+      activeKey={selectedMenuItems(items)[0]}
       mode={'inline'}
       items={items}
     />
   )
 }
 
-export const TasksMenu: FC = () => (
-  <Flex vertical justify={'space-between'} className={styles.menuContainer}>
-    <MenuComponent items={items} />
-    <MenuComponent items={bottomItems} />
-  </Flex>
-)
+export const TasksMenu: FC = () => {
+  return (
+    <Flex vertical justify={'space-between'} className={styles.menuContainer}>
+      <MenuComponent items={items} />
+      <MenuComponent items={bottomItems} />
+    </Flex>
+  )
+}

16ede2fae2101d18c846463e9ea26b252375fe00
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-10-02 12:46:09 +0300

    text deploy

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
   // uri: `http://localhost:3000/api/graphql`,
-  uri: `https://donedealio.vercel.app/api/graphql`,
+  // uri: `https://donedealio.vercel.app/api/graphql`,
+  uri: process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_API_ROOT,
   cache: new InMemoryCache(),
 });
 

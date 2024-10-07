326b0043f8d45350daf99420e777866f3dcdac09
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-10-07 20:16:40 +0300

    add create and update task form for calendar

--- src/components/pages/user/tasks/calendar/index.tsx
+++ src/components/pages/user/tasks/calendar/index.tsx
@@ -1,75 +1,29 @@
 "use client";
 
 import React, { FC, Fragment, useState } from 'react';
-import { App, Badge, Calendar, DatePicker, Flex, Form, Input, Modal, Upload } from 'antd';
-import type { BadgeProps, CalendarProps, UploadProps } from 'antd';
-import type { Dayjs } from 'dayjs';
-import { Button, FormItem } from '@/components/ui';
-import { FileAddOutlined } from '@ant-design/icons'
+import { Calendar, Flex, Form, Modal, Button as AntBtn } from 'antd';
+import type { CalendarProps } from 'antd';
+import { Dayjs } from 'dayjs';
 
-import { Priority, Status, usePrioritiesQuery, useStatusesQuery } from '@/graphql/types';
-import { StatusField } from '@/components/elements';
-
+import { Task, useTasksQuery } from '@/graphql/types';
+import { CreateTaskForm } from '@/components/elements';
+import dayjs from 'dayjs';
+import { useParams } from 'next/navigation';
+import styles from './index.module.scss';
+import { UpdateTaskForm } from '@/components/elements/tasks/update-task-form';
 
 export const CalendarView: FC = () => {
-  const [form] = Form.useForm();
-  const {message} = App.useApp();
+  const createTaskForm = Form.useForm();
+  const updateTaskForm = Form.useForm();
+  const { workspaceId } = useParams();
+  const [selectedTaskId, setTaskId] = useState<string>('')
+  const [selectedDate, setDate] = useState<Dayjs | null>(null)
 
-  const { data: statusesData } = useStatusesQuery();
-  const { data: prioritiesData } = usePrioritiesQuery();
-  const [openedTaskDate, setOpenTaskDate] = useState<Dayjs | null>(null)
-
-  const statusValue= Form.useWatch('status', form);
-  const priorityValue = Form.useWatch('priority', form);
-
-  const props: UploadProps = {
-    name: 'file',
-    action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-    headers: {
-      authorization: 'authorization-text',
-    },
-    onChange(info) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-  };
-
-  const getListData = (value: Dayjs) => {
-    let listData: { type: string; content: string }[] = []; // Specify the type of listData
-    switch (value.date()) {
-      case 8:
-        listData = [
-          { type: 'warning', content: 'This is warning event.' },
-          { type: 'success', content: 'This is usual event.' },
-        ];
-        break;
-      case 10:
-        listData = [
-          { type: 'warning', content: 'This is warning event.' },
-          { type: 'success', content: 'This is usual event.' },
-          { type: 'error', content: 'This is error event.' },
-        ];
-        break;
-      case 15:
-        listData = [
-          { type: 'warning', content: 'This is warning event' },
-          { type: 'success', content: 'This is very long usual event......' },
-          { type: 'error', content: 'This is error event 1.' },
-          { type: 'error', content: 'This is error event 2.' },
-          { type: 'error', content: 'This is error event 3.' },
-          { type: 'error', content: 'This is error event 4.' },
-        ];
-        break;
-      default:
+  const { data: tasksData, refetch } = useTasksQuery({
+    variables: {
+      workspaceId: workspaceId as string
     }
-    return listData || [];
-  };
+  });
 
   const getMonthData = (value: Dayjs) => {
     if (value.month() === 8) {
@@ -87,16 +41,53 @@ export const CalendarView: FC = () => {
     ) : null;
   };
 
+  const onSelectTask = (item: Task, date: Dayjs) => {
+    setTaskId(item?.id)
+
+    updateTaskForm[0].setFieldsValue({
+      task: item?.name,
+      status: item?.status,
+      priority: item?.priority,
+      notes: item?.note,
+      dueDate: date
+    })
+
+  };
+
   const dateCellRender = (value: Dayjs) => {
-    const listData = getListData(value);
+    const formattedDate = value.format('YYYY-MM-DD')
+    const data = tasksData?.tasks?.filter(item => dayjs(item?.dueDate).format('YYYY-MM-DD') === formattedDate)
+
     return (
-      <ul className="events">
-        {listData.map((item) => (
-          <li key={item.content}>
-            <Badge status={item.type as BadgeProps['status']} text={item.content} />
-          </li>
-        ))}
-      </ul>
+      <Flex vertical justify={'space-between'} style={{height: '100%'}}>
+        <ul className={styles.tasksList}>
+          {data?.map((item) => (
+            <li key={item?.id}>
+              <button
+                style={{ background: item?.status?.color }}
+                onClick={(event) => {
+                  event.stopPropagation();
+                  onSelectTask(item as Task, value)
+                }}
+              >
+                {item?.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        {!!data?.length && (
+          <Flex justify='end'>
+            <AntBtn
+              type={'text'}
+              size={'small'}
+              onClick={() => onSelectCell(value)}
+            >
+              + Add
+            </AntBtn>
+          </Flex>
+        )}
+      </Flex>
     );
   };
 
@@ -110,109 +101,48 @@ export const CalendarView: FC = () => {
     console.log(value.format('YYYY-MM-DD'), mode);
   };
 
-  const handleClosetaskForm = () => {
-    form.resetFields();
-    setOpenTaskDate(null);
-  }
- 
-  const hanldeCreateTsk = (values: any) => {
-    try {
-      console.log(values, 'Values')
-    } catch(error) {
-      console.log(error, 'Create task error')
-    } finally {
-      handleClosetaskForm()
-    }
-  }
+  const handleCloseCreateTask = () => setDate(null);
+  const handleCloseUpdateTask = () => setTaskId('');
+
+  const onSelectCell = (date: Dayjs) => {
+    setDate(date);
+    createTaskForm[0].setFieldValue('dueDate', date);
+  };
 
   return (
     <Fragment>
       <Calendar
         onPanelChange={onPanelChange}
         cellRender={cellRender}
-        onSelect={(date) => {
-          setOpenTaskDate(date)
-        }}
+        onSelect={onSelectCell}
       />
       <Modal 
-        open={!!openedTaskDate}
-        onCancel={() => setOpenTaskDate(null)}
-        onOk={form.submit}
-        okButtonProps={{size: 'large'}}
-        cancelButtonProps={{ size: 'large' }}
-        okText={'Create'}
-        confirmLoading={false}
+        open={!!selectedDate}
+        onCancel={handleCloseCreateTask}
         styles={{content: {padding: '58px 32px 32px'}}}
+        footer={false}
         closable
       >
-        <Form 
-          form={form} 
-          onFinish={hanldeCreateTsk}
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          labelAlign={'left'}
-          colon={false}
-          size={'large'}
-        >
-          <Flex vertical>
-            <FormItem
-              name={'task'}
-              label={'Task'}
-            >
-              <Input />
-            </FormItem>
+        <CreateTaskForm 
+          form={createTaskForm[0]}
+          onCloseForm={handleCloseCreateTask} 
+          refetchTasks={refetch}
+        />
+      </Modal>
 
-            <FormItem
-              name={'status'}
-              label={'Status'}
-            >
-              <StatusField
-                form={form}
-                data={statusesData?.statuses as Status[]}
-                status={statusValue as Status}
-                updatedField={'status'}
-              />
-            </FormItem>
-
-            <FormItem
-              name={'dueDate'}
-              label={'Due date'}
-            >
-              <DatePicker
-                format={'DD MMM'}
-                showNow={false}
-                placeholder=''
-                style={{width: '100%'}}
-              />
-            </FormItem>
-
-            <FormItem
-              name={'priority'}
-              label={'Priority'}
-            >
-              <StatusField
-                form={form}
-                data={prioritiesData?.priorities as Priority[]}
-                status={priorityValue as Priority}
-                updatedField={'priority'}
-              />
-            </FormItem>
-            <FormItem
-              name={'notes'}
-              label={'Notes'}
-            >
-              <Input />
-            </FormItem>
-            <FormItem
-              name={'files'}
-              label={'Files'}
-            >
-              <Upload {...props}>
-                <Button icon={<FileAddOutlined style={{color: '#d9d9d9'}} />} type={'default'} ghost />
-              </Upload>
-            </FormItem>
-          </Flex>
-        </Form>
+      <Modal
+        open={!!selectedTaskId}
+        onCancel={() => setTaskId('')}
+        styles={{ content: { padding: '58px 32px 32px' } }}
+        footer={false}
+        closable
+      >
+        <UpdateTaskForm
+          taskId={selectedTaskId}
+          form={updateTaskForm[0]}
+          onCloseForm={handleCloseUpdateTask}
+          refetchTasks={refetch}
+        />
       </Modal>
     </Fragment>
   )

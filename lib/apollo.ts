b7d2e5a00a0903957fdba046363f3c5b482ec1e8
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-10-10 20:39:44 +0300

    test

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,9 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
   uri: 
-    // `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql` || 
-    // `${process.env.NEXT_PUBLIC_API_ROOT}/api/graphql`,
-    'http://localhost:3000/api/graphql',
+    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql` || 
+    `${process.env.NEXT_PUBLIC_API_ROOT}/api/graphql`,
   cache: new InMemoryCache(),
 });
 

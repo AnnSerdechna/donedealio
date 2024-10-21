ad54ab9b72540fef64e8e9cb488ce3cd73b022cb
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-10-21 13:20:04 +0300

    fixed deploy errors

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,15 +1,19 @@
+import { Suspense } from 'react';
+
 import { AuthCard, RegisterForm } from '@/components/auth';
 
 export default function Register ()  {
   return (
-    <AuthCard
-      title = { 'Sign up'}
-      description = { 'Already have account?'}
-      backLinkUrl = { '/auth/login'}
-      backLinkLabel = { 'Sign in'}
-      hasSocials
-    >
-    <RegisterForm />
-  </AuthCard >
+    <Suspense fallback={null}>
+      <AuthCard
+        title={'Sign up'}
+        description={'Already have account?'}
+        backLinkUrl={'/auth/login'}
+        backLinkLabel={'Sign in'}
+        hasSocials
+      >
+        <RegisterForm />
+      </AuthCard >
+    </Suspense>
   )
 }

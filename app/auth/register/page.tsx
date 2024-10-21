2a051c886e96f48d83339bd73cec78dd40442186
Анна Сердечна <annaserdecna@MacBook-Air-Anna.local>
2024-10-21 14:44:42 +0300

    test send mail on prod

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,19 +1,11 @@
 import { Suspense } from 'react';
 
-import { AuthCard, RegisterForm } from '@/components/auth';
+import { RegisterForm } from '@/components/auth';
 
-export default function Register ()  {
+export default function Register() {
   return (
     <Suspense fallback={null}>
-      <AuthCard
-        title={'Sign up'}
-        description={'Already have account?'}
-        backLinkUrl={'/auth/login'}
-        backLinkLabel={'Sign in'}
-        hasSocials
-      >
-        <RegisterForm />
-      </AuthCard >
+      <RegisterForm />
     </Suspense>
   )
 }
